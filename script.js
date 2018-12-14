f38b34322de045ea9c07899681316c7459eb146e
alicemitali <alicemitali6@gmail.com>
2018-12-14 13:48:59 +0200

    commit msg

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var sideSum1 = function(firstvalue, secondvalue) {
     return firstvalue + secondvalue;
-  };var sideSum2 = function(secondvalue, thirdvalue) {
+  };
+  var sideSum2 = function(secondvalue, thirdvalue) {
     return secondvalue + thirdvalue;
+  };
+  var sideSum3 = function(firstvalue, thirdvalue) {
+    return firstvalue + thirdvalue;
   };
