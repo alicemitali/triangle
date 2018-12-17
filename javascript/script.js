db728aa3ad029f101b1cd5b4d876325ce326ded9
alicemitali <alicemitali6@gmail.com>
2018-12-17 11:42:27 +0200

    last change

---
+++ javascript/script.js
@@ -0,0 +1,46 @@
+var sideSum1 = function(firstvalue, secondvalue) {
+  return firstvalue + secondvalue;
+};
+var sideSum2 = function(secondvalue, thirdvalue) {
+  return secondvalue + thirdvalue;
+};
+var sideSum3 = function(firstvalue, thirdvalue) {
+  return firstvalue + thirdvalue;
+};
+function check() {
+  var firstvalue = parseInt(document.getElementById("one").value);
+  var secondvalue = parseInt(document.getElementById("two").value);
+  var thirdvalue = parseInt(document.getElementById("three").value);
+
+  var text;
+  if (
+    sideSum1(firstvalue, secondvalue) <= thirdvalue ||
+    sideSum2(secondvalue, thirdvalue) <= firstvalue ||
+    sideSum3(firstvalue, thirdvalue) <= secondvalue
+  ) {
+    text = "it is not triangle";
+  } else if (firstvalue === secondvalue && secondvalue === thirdvalue) {
+    text = "Equilaterial Triangle.";
+  }
+  //Equivalent Triangle
+  else if (
+    firstvalue === secondvalue ||
+    firstvalue === thirdvalue ||
+    secondvalue === thirdvalue
+  ) {
+    text = "Isoscele Triangle.";
+  }
+  //Isoscele Triangle
+  else if (
+    firstvalue !== secondvalue &&
+    firstvalue !== thirdvalue &&
+    secondvalue !== thirdvalue
+  ) {
+    text = "Scalene Triangle.";
+  }
+  //Scalene Triangle
+  else {
+    text = "Not a Triangle.";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
